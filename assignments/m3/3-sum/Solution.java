8159b891d972c71e5ffa8906a74b0078e68ac4ff
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-09-26 23:53:20 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

---
+++ assignments/m3/3-sum/Solution.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Solution {
+
+    final public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int arraysize = scan.nextInt();
+        int[] array = new int[arraysize];
+        int count = 0;
+        for (int i = 0; i < arraysize; i++) {
+            array[i] = scan.nextInt();
+        }
+        Arrays.sort(array);
+        int j = 0;
+        int k = 0;
+        for (int i = 0; i < arraysize; i++) {
+            j = i + 1;
+            k = arraysize - 1;
+            while (j < k) {
+                if (array[i] + array[j] + array[k] < 0) {
+                    j += 1;
+                } else if (array[i] + array[j] + array[k] > 0) {
+                    k -= 1;
+                } else {
+                    j += 1;
+                    k -= 1;
+                    count += 1;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
