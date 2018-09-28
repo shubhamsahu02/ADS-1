9d065f583ccb686a8b60e28f1f446d999ade731e
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-09-28 23:54:14 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

---
+++ assignments/m4/4.1 Balanced Paranthesis/Solution.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+
+class LinkedList {
+
+    private class Node {
+
+        private char data;
+
+        private Node nextadrr;
+    }
+ 
+    private Node head = null;
+
+    public void push(final char item) {
+        Node temp = new Node();
+        temp.data = item;
+        temp.nextadrr = head;
+        head = temp;
+    }
+
+    public char pop() {
+        char data = head.data;
+        head = head.nextadrr;
+        return  data;
+    }
+
+    public boolean isEmpty() {
+        return  head == null;
+    }
+
+    public char top() {
+        return head.data;
+    }
+
+}
+
+public final class Solution {
+
+    private Solution() {}
+
+    public static void main (String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int k = Integer.parseInt(scan.nextLine());
+        for (int i = 0; i < k; i++) {
+            String s = scan.next();
+            if (theparantheses(s)) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+
+    public static boolean theparantheses(final String s) {
+        LinkedList l = new LinkedList();
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            char ch = s.charAt(i);
+            if (ch == '{' || ch == '(' || ch == '[') {
+                l.push(ch);
+            } else {
+                if (l.isEmpty()) {
+                    return false;
+                }
+                if (ch == '}' && l.top() == '{') {
+                    l.pop();
+                } else if (ch == ']' && l.top() == '[') {
+                    l.pop();
+                } else if (ch == ')' && l.top() == '(') {
+                    l.pop();
+                } else {
+                    return false;
+                }
+
+            }
+        }
+        return l.isEmpty();
+    }
+}
