e295fe8419a56722a6f9d48b88607f43e8af48b3
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-10-07 23:36:36 +0530

    "ads1_m5_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m5/5.1 Deque/LinkedList.java
+++ assignments/m5/5.1 Deque/LinkedList.java
@@ -1,23 +1,41 @@
 class LinkedList {
+    
     private int length = 0;
-
+    /**
+     Node object first.
+     **/
     private Node first = null;
-
+    /**
+     Node object last.
+     **/
     private Node last = null;
-
+    /**
+     class node for the linked list.
+     **/
     private class Node {
-
+        /**
+         char item.
+         **/
         private int item;
-
+        /**
+         Node object.
+         **/
         private Node next;
     }
-
+    /**
+     to check if the stack is empty.
+     @return bool value.
+     **/
     public boolean isEmpty() {
         return first == null;
     }
-
+    /**
+     used to push the item.
+     * @param item int.
+     **/
     public void pushAtStart(final int item) {
         if (first == null) {
+            //Node oldfirst = first;
             first = new Node();
             first.item = item;
             first.next = null;
@@ -45,7 +63,10 @@ class LinkedList {
         }
         length++;
     }
-
+    /**
+     * used to pop the item from the stack.
+     * @return string pop
+     **/
     public void popAtStart() {
         if (first != null) {
             first = first.next;
@@ -64,7 +85,10 @@ class LinkedList {
             length--;
         }
     }
-
+    /**
+     used to check the item on the top.
+     * @return first item.
+     **/
     public int top() {
         return first.item;
     }
