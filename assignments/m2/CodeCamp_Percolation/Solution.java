68bf0586640a9619a4272cd992669aa6d71eddb2
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-10-27 02:17:09 +0530

    "ads1_m2_a1: 24/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m2/CodeCamp_Percolation/Solution.java
+++ assignments/m2/CodeCamp_Percolation/Solution.java
@@ -1,62 +1,101 @@
-/**.
- * Imports Scanner package
- */
 import java.util.Scanner;
-/**.
- * Class for solution.
- */
-public final class Solution {
-    /**.
-     * Constructs the object for Soluiton class
-     */
+/**
+ * class.
+ **/
+final class Solution {
+    /**
+     constructor.
+     **/
     private Solution() {
+        /**
+         constructor.
+         **/
+    }
+    /**
+     main function.
+     * @param args String.
+     **/
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = Integer.parseInt(sc.nextLine());
+        Percolate obj = new Percolate(input);
+        while (sc.hasNextLine()) {
+            String[] inputs = sc.nextLine().split(" ");
+            obj.open(Integer.parseInt(inputs[0]) - 1,
+                Integer.parseInt(inputs[1]) - 1);
+        }
+        System.out.println(obj.isPercolate());
+    }
+
+}
+/**
+ * class.
+ **/
+class Percolate {
+    /** grid boolean.
+    **/
+    private boolean[][] grid;
+    /** object.
+    **/
+    private WeightedQuickUnionUF obj;
+    /**
+     * int size.
+    **/
+    private int size;
+    /**
+     * @param size1 int.
+     **/
+    Percolate(final int size1) {
+        this.size = size1;
+        grid = new boolean[size][size];
+        obj = new WeightedQuickUnionUF(size * size + 2);
+    }
+    /**
+     * @param row int.
+     * @param column int.
+     **/
+    public void open(final int row, final int column) {
+        if (grid[row][column]) {
+            return;
+        }
+        grid[row][column] = true;
+        if (row == 0) {
+            obj.union(convert(row, column), size * size);
+        }
+        if (row == size - 1) {
+            obj.union(convert(row, column), size * size + 1);
+        }
+        if (row < size - 1 && grid[row + 1][column]) { //bottom element
+            obj.union(convert(row, column),
+                convert(row + 1, column));
+        }
+        if (row > 0 && grid[row - 1][column]) { //top element
+            obj.union(convert(row, column),
+                convert(row - 1, column));
+        }
+        if (column > 0 && grid[row][column - 1]) { //left element
+            obj.union(convert(row, column),
+             convert(row, column - 1));
+        }
+        if (column < size - 1 && grid[row][column + 1]) {
+            obj.union(convert(row, column),
+             convert(row, column + 1));
+        }
+
 
     }
-    /**.
-     * main function which demonstrates the operations of linked list.
-     *
-     * @param      args  The arguments
-     */
-    public static void main(final String[] args) {
-        Scanner scan = new Scanner(System.in);
-        LinkedList ll = new LinkedList();
-        while (scan.hasNext()) {
-            String[] line = scan.nextLine().split(" ");
-            switch (line[0]) {
-                case "size":
-                System.out.println(ll.size());
-                break;
-                case "pushLeft":
-                ll.pushLeft(Integer.parseInt(line[1]));
-                System.out.println(ll);
-                break;
-                case "pushRight":
-                ll.pushRight(Integer.parseInt(line[1]));
-                System.out.println(ll);
-                break;
-                case "removeLeft":
-                ll.removeLeft();
-                System.out.println(ll);
-                break;
-                case "popLeft":
-                ll.popLeft();
-                System.out.println(ll);
-                break;
-                case "removeRight":
-                ll.removeRight();
-                System.out.println(ll);
-                break;
-                case "print" :
-                ll.toString();
-                System.out.println(ll);
-                break;
-                case "remove":
-                ll.remove(Integer.parseInt(line[1]));
-                System.out.println(ll);
-                //ll.toString();
-                default:
-                break;
-            }
-        }
+    /**
+    * @return true or false.
+     **/
+    public boolean isPercolate() {
+        return obj.connected(size * size, size * size + 1);
+    }
+/**
+ * @param i int.
+ * @param j int.
+ * @return i*size+j.
+ **/
+    public int convert(final int i, final int j) {
+        return i * size + j;
     }
 }
