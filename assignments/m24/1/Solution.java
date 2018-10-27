e7976b05927ffaf4b6df36af3e7da7d2054b1784
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-10-27 13:06:43 +0530

    "m24_a1_1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m24/1/Solution.java
+++ assignments/m24/1/Solution.java
@@ -24,20 +24,29 @@ class Solution {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int putnum = Integer.parseInt(scan.nextLine());
-		SeparateChainingHashST<Integer, Student> schsobj= new SeparateChainingHashST<>();
-		for(int i = 0; i < putnum; i++) {
+		SeparateChainingHashST<Integer, Student> schsobj = new SeparateChainingHashST<>();
+		for (int i = 0; i < putnum; i++) {
 			String[] tokens = scan.nextLine().split(",");
 			Student studentobj = new Student(tokens[1], Double.parseDouble(tokens[2]));
 			schsobj.put(Integer.parseInt(tokens[0]), studentobj);
 		}
 		int querynum = Integer.parseInt(scan.nextLine());
-		for(int j = 0; j < querynum; j++) {
+		for (int j = 0; j < querynum; j++) {
 			String[] querytokens = scan.nextLine().split(" ");
-			if(querytokens[0].equals("get")) {
-				if(Integer.parseInt(querytokens[2]) == 1) {
-					System.out.println(schsobj.get(Integer.parseInt(querytokens[1])).getname());
+			if (querytokens[0].equals("get")) {
+				if (Integer.parseInt(querytokens[2]) == 1) {
+					if (schsobj.get(Integer.parseInt(querytokens[1])) == null) {
+						System.out.println("Student doesn't exists...");
+					} else {
+						System.out.println(schsobj.get(Integer.parseInt(querytokens[1])).getname());
+					}
+
 				} else {
-					System.out.println(schsobj.get(Integer.parseInt(querytokens[1])).getmarks());
+					if (schsobj.get(Integer.parseInt(querytokens[1])) == null) {
+						System.out.println("Student doesn't exists...");
+					} else {
+						System.out.println(schsobj.get(Integer.parseInt(querytokens[1])).getmarks());
+					}
 				}
 			}
 		}
