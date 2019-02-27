cc3acf8fe84002db3e639d29cb0c857ee287b7d6
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2019-02-28 03:03:24 +0530

    "ads1_m6_a1: 12/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS/Assignment-1/LinkedList.java
+++ ADS/Assignment-1/LinkedList.java
@@ -1,52 +1,125 @@
-class LinkedList {
-    Node start = null;
-    Node end = null;
-    int size = 0;
-
-    class Node {
-        String item;
+public class LinkedList {
+    /**
+     * Class for node.
+     */
+    private class Node {
+        int data;
         Node next;
-        Node(String item) {
-            this.item = item;
+        /**
+         * Constructs the object.
+         */
+        Node() {
+
+        }
+        /**
+         * Constructs the object.
+         *
+         * @param      data  The data
+         */
+        Node(int data) {
+            this.data = data;
+        }
+        /**
+         * Constructs the object.
+         *
+         * @param      data  The data
+         * @param      n     { parameter_description }
+         */
+        Node(int data, Node n) {
+            this.data = data;
+            this.next = n;
         }
     }
+    private Node head, tail;
+    private int size;
+    /**
+     * Constructs the object.
+     */
+    LinkedList() {
 
-    public boolean isEmpty() {
-        return size == 0;
     }
-    
-    public void push(String str) {
-        Node newnode = new Node(str);
-        if(isEmpty()) {
-            start = newnode;
-            end = start;
-            end.next = null;
+    /**
+     * Pushes an object at tail.
+     *
+     * @param      data  The data
+     */
+    public void push(int data) {
+        if (head == null) {
+            head = new Node(data);
+            tail = head;
             size++;
             return;
         }
-
-        Node old = end;
-        end = newnode;
-        old.next = end;
-        end.next = null;
+        Node node = new Node(data, null);
+        tail.next = node;
+        tail = node;
         size++;
     }
-    
-    public String pop() {
-        if(isEmpty()) {
-            return null;
+    /**
+     * Pushes at head.
+     *
+     * @param      data  The data
+     */
+    public void pushHead(int data) {
+        if (head == null) {
+            head = new Node(data);
+            tail = head;
+            size++;
+            return;
         }
-        String item = start.item;
-        start = start.next;
-        size--;
-        return item;
+        Node node = new Node(data, head);
+        head = node;
+        size++;
     }
-
-    public void printList() {
-        Node temp = start;
-        while (temp != null) {
-            System.out.print(temp.item);
-            temp = temp.next;
+    /**
+     * Pops out the object.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int pop() {
+        if (head != null) {
+            Node temp = head;
+            if(size > 1) {
+                while (temp.next != tail) {
+                    temp = temp.next;
+                }
+            }
+            int val = tail.data;
+            temp.next = null;
+            tail = temp;
+            size--;
+            return val;
         }
+        return 0;
+    }
+    /**
+     * Determines if empty.
+     *
+     * @return     True if empty, False otherwise.
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
+    public String toString() {
+        Node thead = head;
+        String s = "";
+        while (thead != null) {
+            s += thead.data;
+            thead = thead.next;
+        }
+        return s;
+    }
+    /**
+     * Gets the size.
+     *
+     * @return     The size.
+     */
+    public int getSize() {
+        return size;
     }
 }
--- ADS/Assignment-1/Solution.java
+++ ADS/Assignment-1/Solution.java
@@ -1,65 +1,85 @@
 import java.util.Scanner;
-/**
- * Class for add large numbers.
- */
-final class AddLargeNumbers {
-    /**
-     * Constructs the object.
-     */
 
-    public static LinkedList numberToDigits(final String number) {
+ class AddLargeNumbers {
+
+    private static  int NINE = 9;
+ 
+    private AddLargeNumbers() {
+
+    }
+
+    public static LinkedList numberToDigits( String number) {
         LinkedList l = new LinkedList();
         String[] numbers = number.split("");
         for (int i = 0; i < numbers.length; i++) {
-            l.push(numbers[i]);
+            l.push(Integer.parseInt(numbers[i]));
         }
         return l;
     }
-    /**
-     * Returns string of linked list.
-     *
-     * @param      list  The list
-     *
-     * @return     { string of link list }
-     */
-    public static String digitsToNumber(final LinkedList list) {
-        // return list.toString();
-        String string = "";
-        while (!list.isEmpty()) {
-            string += list.pop();
-        }
 
-        return string;
+    public static String digitsToNumber( LinkedList list) {
+        return list.toString();
     }
-    /**
-     * Adds large numbers.
-     *
-     * @param      list1  The list 1
-     * @param      list2  The list 2
-     *
-     * @return     { linkedlist of sum }
-     */
-    public static LinkedList addLargeNumbers(final LinkedList list1, final LinkedList list2)
-    {
-return null;
+
+    public static LinkedList addLargeNumbers( LinkedList list1,
+         LinkedList list2) {
+        LinkedList sum = new LinkedList();
+        LinkedList carry = new LinkedList();
+        LinkedList a, b;
+        int c = 0;
+        if (list1.getSize() >= list2.getSize()) {
+            a = list1;
+            b = list2;
+        } else {
+            a = list2;
+            b = list1;
+        }
+        while (!(b.isEmpty())) {
+            if (carry.isEmpty()) {
+                c = a.pop() + b.pop();
+            } else {
+                c = a.pop() + b.pop() + carry.pop();
+            }
+            if (c > NINE) {
+                String[] num = Integer.toString(c).split("");
+                sum.pushHead(Integer.parseInt(num[1]));
+                carry.pushHead(Integer.parseInt(num[0]));
+                c = 0;
+            } else {
+                sum.pushHead(c);
+                c = 0;
+            }
+        }
+        if (!(carry.isEmpty())) {
+            if (a.isEmpty()) {
+                c = carry.pop();
+            } else {
+                c = a.pop() + carry.pop();
+            }
+            if (c > NINE) {
+                String[] num = Integer.toString(c).split("");
+                sum.pushHead(Integer.parseInt(num[1]));
+                carry.pushHead(Integer.parseInt(num[1]));
+                c = 0;
+            } else {
+                sum.pushHead(c);
+                c = 0;
+            }
+        }
+        while (!(a.isEmpty())) {
+            sum.pushHead(a.pop());
+        }
+        return sum;
     }
 }
-/**
- * Class for solution.
- */
-public final class Solution {
-    /**
-     * Constructs the object.
-     */
+
+public  class Solution {
+
     private Solution() {
 
     }
-    /**
-     * Main method to take inputs and return outputs.
-     *
-     * @param      args  The arguments
-     */
-    public static void main(final String[] args) {
+
+    public static void main( String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         String p = sc.nextLine();
@@ -75,7 +95,8 @@ public final class Solution {
         case "addLargeNumbers":
             pDigits = AddLargeNumbers.numberToDigits(p);
             qDigits = AddLargeNumbers.numberToDigits(q);
-            LinkedList result = AddLargeNumbers.addLargeNumbers(pDigits, qDigits);
+            LinkedList result = AddLargeNumbers.addLargeNumbers(
+                pDigits, qDigits);
             System.out.println(AddLargeNumbers.digitsToNumber(result));
             break;
         default:
