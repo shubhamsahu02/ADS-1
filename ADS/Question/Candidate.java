46f8e69abf522c73fa608602f7dc3f971ea9f2ec
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2019-03-08 12:15:53 +0530

    "ADS-1 remedial-5: 0/7 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS/Question/Candidate.java
@@ -0,0 +1,81 @@
+class Candidate implements Comparable<Candidate> {
+
+	String name, degree;
+	int studid;
+	double percentage;
+
+public Candidate(int id, String name, String degree, double perecentage) {
+	this.studid = studid;
+	this.name = name;
+	this.degree = degree;
+	this.percentage = percentage;
+}
+
+	public int getId() {
+		return studid;
+	}
+
+	public void setId(int id) {
+		this.studid = studid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName() {
+		this.name = name;
+	}
+
+	public String getDegree() {
+		return degree;
+	}
+
+	public void setDegree() {
+		this.degree = degree;
+	}
+
+	public Double getPercentage() {
+		return percentage;
+	}
+
+	public void setPercetage() {
+		this.percentage = percentage;
+	}
+
+	public int compareTo(Candidate that) {
+		if(this.percentage > that.percentage) {
+			return 1;
+		}
+
+		if (this.percentage < that.percentage) {
+			return -1;
+		}
+
+		if(this.studid < that.studid) {
+			return 1;
+		}
+
+		if(this.studid > that.studid) {
+			return -1;
+		}
+
+		else {
+			return 0;
+		}
+
+
+
+	}
+		public String toString(){
+		String string = " ";
+		string = string + this.studid + this.degree + this.percentage;
+		return string;
+
+
+	}
+
+
+
+
+}
---
+++ ADS/Question/Solution.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = Integer.parseInt(scan.nextLine());
+		Candidate candidate;
+		MaxPQ<Candidate> pq = new MaxPQ<>();
+		while (scan.hasNext()) {
+			String line = scan.nextLine();
+			String[] tokens = line.split(" ");
+
+			switch (tokens[0]) {
+
+				case "Apply" :
+					String[] tokens1 = tokens[1].split(",");
+				 candidate = new Candidate(Integer.parseInt(tokens[0]),tokens[1], tokens[2], Double.parseDouble(tokens[3]));
+				 pq.insert(candidate);
+				break;
+
+				case "Recruit_Top_Candidate" :
+				System.out.println(pq.delMax());
+				break;
+
+				case "Ranking" :
+				System.out.println();
+				int i = 0;
+				while(pq.size() != 0) {
+					i++;
+					System.out.println(i+1 + ":" + pq.delMax());
+				}
+			}
+
+		}
+
+	}
+}
