f6e7efdf7a8e5bf302868e6d24ccc5916a3a9404
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2019-06-09 17:36:53 +0530

    "ADS-1 remedial-5: 4/7 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS/Question/sol.java
@@ -0,0 +1,98 @@
+import java.util.*;
+
+public class sol {
+
+
+	public static void main(String[] args) {
+		Scanner scan  = new Scanner(System.in);
+		MaxPQ <Student> max = new MaxPQ<>(); 
+		Student stud;
+		int n = scan.nextInt();
+		scan.nextLine();
+		while(scan.hasNext()) {
+			String[] str = scan.nextLine().split(" ");
+
+			if(str[0].equals("Apply")) {
+				String[] str1 = str[1].split(",");
+				stud = new Student(Integer.parseInt(str1[0]), str1[1], str1[2], Double.parseDouble(str1[3]));
+				max.insert(stud);
+			}
+
+			else if (str[0].equals("Recruit_Top_Candidate")) {
+				System.out.println(max.delMax());	
+			}
+
+			else if (str[0].equals("Ranking")) {
+				for (int i = 0; i < max.size(); i++) {
+				System.out.println(i+1 + ":" + max.delMax());
+			}
+
+			}
+		}
+
+	}
+
+}
+
+
+class Student  implements Comparable <Student> {
+	int id;
+	String name;
+	String branch;
+	double marks;
+
+	public	Student(int id, String name, String branch,double marks) {
+			this.id = id;
+			this.name = name;
+			this.branch = branch;
+			this.marks = marks;
+		}
+
+		public int getId() {
+			return id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public String getBranch() {
+			return branch;
+		}
+
+		public Double getMarks() {
+			return marks;
+		}
+
+
+		public int compareTo(Student that) {
+
+			if (this.marks > that.marks){
+				return 1;
+			}
+
+			if (this.marks < that.marks) {
+				return -1;
+			}
+
+			if(this.id > that.id) {
+				return 1;
+			}
+
+			if (this.id < that.id) {
+				return -1;
+			}
+
+			else {
+				return 0;
+			}
+
+		}
+
+		public String toString() {
+			String string = "";
+			string  = string + this.id + "," + this.name + "," + this.branch + "," + this.marks; 
+			return string;
+		}
+
+}
