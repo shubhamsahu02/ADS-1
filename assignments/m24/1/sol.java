f4cdb56b02307c55830fced3696096290fffbd16
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2019-06-13 00:35:18 +0530

    "m24_a1_1: 1/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m24/1/sol.java
+++ assignments/m24/1/sol.java
@@ -24,20 +24,38 @@ class sol {
 		for (int j = 0; j < query+1; j++) {
 			String[] tokens = scan.nextLine(). split(" ");
 			if (tokens[0].equals("get")) {
-				if(Integer.parseInt(tokens[2]) == 1) {
-					 System.out.println(name.get(Integer.parseInt(tokens[1])));
-				}
-				// else if (name.get(Integer.parseInt(tokens[2])) == null) {
-				// 	 System.out.println("Student doesn't exists...");
-				// }
 
-				else if(Integer.parseInt(tokens[2]) == 2) {
+				if(name.get(Integer.parseInt(tokens[1])) != null && tokens[2].equals("1")) {
+					 System.out.println(name.get(Integer.parseInt(tokens[1])));
+			// 	}
+				}
+
+				if(marks.get(Integer.parseInt(tokens[1])) != null && tokens[2].equals("2")) {
+					 // System.out.println(name.get(Integer.parseInt(tokens[1])));
+			// 	}
+
 					 System.out.println(marks.get(Integer.parseInt(tokens[1])));
 				}
 
-				else if(name.get(Integer.parseInt(tokens[2])) == null || marks.get(Integer.parseInt(tokens[2])) == null) {
+				else {
 					System.out.println("Student doesn't exists...");
-			}
+			// }
+				}
+				
+			// 	if(Integer.parseInt(tokens[2]) == 1) {
+			// 		 System.out.println(name.get(Integer.parseInt(tokens[1])));
+			// 	}
+			// 	// else if (name.get(Integer.parseInt(tokens[2])) == null) {
+			// 	// 	 System.out.println("Student doesn't exists...");
+			// 	// }
+
+			// 	else if(Integer.parseInt(tokens[2]) == 2) {
+			// 		 System.out.println(marks.get(Integer.parseInt(tokens[1])));
+			// 	}
+
+			// 	else if(name.get(Integer.parseInt(tokens[2])) == null || marks.get(Integer.parseInt(tokens[2])) == null) {
+			// 		System.out.println("Student doesn't exists...");
+			// }
 
 			}
 		}
