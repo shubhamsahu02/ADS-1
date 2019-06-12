c1e0ba60182ef5489ad77ff233facd600b0f7db9
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2019-06-13 00:29:29 +0530

    "m24_a1_1: 2/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m24/1/sol.java
+++ assignments/m24/1/sol.java
@@ -27,15 +27,15 @@ class sol {
 				if(Integer.parseInt(tokens[2]) == 1) {
 					 System.out.println(name.get(Integer.parseInt(tokens[1])));
 				}
-				else if (name.get(Integer.parseInt(tokens[2])) == null) {
-					 System.out.println("Student doesn't exists...");
-				}
+				// else if (name.get(Integer.parseInt(tokens[2])) == null) {
+				// 	 System.out.println("Student doesn't exists...");
+				// }
 
 				else if(Integer.parseInt(tokens[2]) == 2) {
 					 System.out.println(marks.get(Integer.parseInt(tokens[1])));
 				}
 
-				else {
+				else if(name.get(Integer.parseInt(tokens[2])) == null || marks.get(Integer.parseInt(tokens[2])) == null) {
 					System.out.println("Student doesn't exists...");
 			}
 
