6584333287d73f2eefce82500d04fd9b6c2e3bbd
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2019-06-13 00:05:56 +0530

    "m24_a1_1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m24/1/sol.java
+++ assignments/m24/1/sol.java
@@ -1,74 +1,42 @@
-import java.util.HashMap;
 import java.util.*;
+import java.util.HashMap;
 
+class sol {
 
-public class sol {
+	sol() {
+
+	}
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int n = Integer.parseInt(scan.nextLine());
-		HashMap<Integer, String> name = new HashMap<Integer, String>();
-		HashMap<Integer, Double> marks = new HashMap<Integer, Double>(); 
+		HashMap<Integer, String> name = new HashMap<>();
+		HashMap<Integer, Double> marks = new HashMap<>();
+		int n = scan.nextInt();
 		for (int i = 0; i < n; i++) {
-			String[] str = scan.nextLine().split(",");
-			name.put(Integer.parseInt(str[0]), str[1]);
-			marks.put(Integer.parseInt(str[0]), Double.parseDouble(str[2]));
+			String[] token = scan.nextLine().split(" ");
+			name.put(Integer.parseInt(token[0]), token[1]);
+			marks.put(Integer.parseInt(token[0]), Double.parseDouble(token[1]));
 		}
+		int query = scan.nextInt();
+		for (int j = 0; j < query; j++) {
+			String[] tokens = scan.nextLine(). split(" ");
+			if (tokens[0] == "get") {
+				if(Integer.parseInt(tokens[2]) == 1) {
+					 System.out.println(name.get(tokens[1]));
+				}
 
-		int k = Integer.parseInt(scan.nextLine());
-		for (int j = 0; j < k; j++) {
-			String[] query = scan.nextLine().split(" ");
-			switch (query[2]) {
-				case "1" :
-				if(name.get(Integer.parseInt(query[1])) == null) {
+				else {
 					System.out.println("Student doesn't exists...");
 				}
-				else {
-				System.out.println(name.get(Integer.parseInt(query[1])));
+				if(Integer.parseInt(tokens[2]) == 2) {
+					 System.out.println(marks.get(tokens[1]));
 				}
-				break;
 
-				case "2" :
-				if(marks.get(Integer.parseInt(query[1])) == null) {
+				else {
 					System.out.println("Student doesn't exists...");
-				}
-				else {
-				System.out.println(marks.get(Integer.parseInt(query[1])));
-				}
-				break;
-
-				default :
-				System.out.println("Student doesn't exists...");
-				break;
+			}
 
 			}
 		}
 	}
-
-	}
-
-	class Student {
-
-		String name;
-		Double marks;
-
-		Student(String name, Double marks) {
-
-			this.name = name;
-			this.marks = marks;
-		}
-
-		public String getName() {
-			return name;
-		}
-
-		public Double getMarks() {
-			return marks;
-		}
-
-		public String toString() {
-			String str = "";
-			str = str + this.name + this.marks;
-			return str;
-		}
-	}
+}
