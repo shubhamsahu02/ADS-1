7d768414dc2550946017631c3decd60a3191c3df
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2019-06-17 01:54:55 +0530

    "ads1_remedial_3_a1_1: 0/7 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- Remedial-3/sol2.java
+++ Remedial-3/sol2.java
@@ -1,45 +1,50 @@
 import java.util.*;
 
-public class sol2{
+public class sol2 {
 
 	sol2() {
 
 	}
 
-
 	public static void main(String[] args) {
-		Scanner scan  = new Scanner (System.in);
+		Scanner scan = new Scanner(System.in);
 		int range = Integer.parseInt(scan.nextLine());
 		Sorting sort = new Sorting();
-		for (int i = 0; i < range; i++) {
-			String[] tokens = scan.nextLine().split(",");
-			Student stud = new Student(Integer.parseInt(tokens[0]), tokens[1], Double.parseDouble(tokens[2]));
+		for(int i = 0; i < range; i++) {
+			String[] line = scan.nextLine().split(",");
+			Student stud = new Student(Integer.parseInt(line[0]), line[1], Double.parseDouble(line[2]));
 			sort.add(stud);
 		}
-		sort.SelectionSort();
-		String temp = sort.display();
-		// System.out.println(temp);
 
-		String[] temp1 = temp.split(",");
-		// System.out.println(temp1[0]);
+		sort.SelectionSort();
 
 		int q = Integer.parseInt(scan.nextLine());
-		for (int i = 0; i < q; i++) {
-			double num = Double.parseDouble(scan.nextLine());
+		String temp = sort.display();
+
+		String[] temp1 = temp.split(",");
 
 			int c = 0;
-			for (String each: temp1) {
-				// System.out.println(each+" im each");
-				String[] abc = each.split(" ");
-				if(Double.parseDouble(abc[2]) == num) {
-					System.out.println(abc[0] +"," + abc[1] +"," + abc[2]);
-					c = 1;
-				}
+		for (int i = 0; i < q; i++) {
+			int query = Integer.parseInt(scan.nextLine());
+			for(String each : temp1) {
+				System.out.println(each);
+			// if(Double.parseDouble(temp1[2]) == num) {
+				// System.out.println(each);
+				// c = 1;
+			// }
+
+			if (c == 0) {
+				System.out.println("This marks are not awarded to any student");
 			}
-			if(c == 0) {
-					System.out.println("This marks are not awarded to any student");
-				}
+
+
+		}	
 		}
+
+
+
 	}
+
+
 
 }
